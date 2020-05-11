805936b3813fdcd9a9307726a3676e0714a663ca
qingyi <qingyi@dtstack.com>
2020-05-11 18:05:53 +0800

    build(git): gitignore lib
    gitignore lib

--- lib/go-back/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export default class GoBack extends React.Component<any, any> {
-    go: () => void;
-    getButtonView(): JSX.Element;
-    render(): JSX.Element;
-}
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Circle } from './circle';
-export { default as GoBack } from './go-back';
-export { default as SlidePane } from './slidePane';
