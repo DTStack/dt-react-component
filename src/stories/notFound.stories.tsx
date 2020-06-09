91587e9fbca9559b4a803a6c30db05887e8a8042
qingyi <qingyi@dtstack.com>
2020-06-09 19:01:54 +0800

    feat(component): add not found component and update goback
    add not found component and update goback

---
+++ src/stories/notFound.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import NotFound from '../components/notFound';
+
+import '../styles/index.scss';
+const stories = storiesOf('NotFound 未匹配', module);
+stories.add('notFound', () => {
+    return (
+        <div className='story_wrapper'>
+            <h2>何时使用</h2>
+            <p>路由未匹配上的展示页</p>
+            <h2>示例</h2>
+            <NotFound/>
+        </div>
+    )
+}, {
+    info: {
+        text: `
+        代码示例：
+        ~~~js
+            <Route path="/*" component={NotFund} />
+        ~~~
+        `
+    }
+})
