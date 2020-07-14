cbc76dcb048c99d59b25a14c01b54cb8c3a153fe
jeromeZhang <jingyu@dtstack.com>
2020-07-14 16:38:32 +0800

    feat: add dtc

--- src/components/globalLoading/index.tsx
+++ src/components/globalLoading/index.tsx
@@ -11,18 +11,18 @@ export default class GlobalLoading extends React.Component<any, any> {
             titleColor = '#fff'
         } = this.props;
         return (
-            <div className="laoding-wrapper" style={{ background: mainbackground }}>
-                <div className="loading-center">
+            <div className="dtc-loading-wrapper" style={{ background: mainbackground }}>
+                <div className="dtc-loading-center">
                     <h1 style={{ color: titleColor }}>{`${prefix}  ${loadingTitle}`}</h1>
-                    <div className="bouncywrap">
-                        <div className="dotcon dc1">
-                            <div className="dot" style={{ background: circleBackground }}></div>
+                    <div className="dtc-bouncywrap">
+                        <div className="dtc-dotcon dtc-dc1">
+                            <div className="dtc-dot" style={{ background: circleBackground }}></div>
                         </div>
-                        <div className="dotcon dc2">
-                            <div className="dot" style={{ background: circleBackground }}></div>
+                        <div className="d t cdotcon dtc-dc2">
+                            <div className="dtc-dot" style={{ background: circleBackground }}></div>
                         </div>
-                        <div className="dotcon dc3">
-                            <div className="dot" style={{ background: circleBackground }}></div>
+                        <div className="dtc-dotcon dtc-dc3">
+                            <div className="dtc-dot" style={{ background: circleBackground }}></div>
                         </div>
                     </div>
                 </div>
