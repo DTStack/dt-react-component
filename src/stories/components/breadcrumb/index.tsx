6cd091d5049e76f81b9e8c2e431dd6a56b9ee1d7
mumiao@dtstack.com <mumiao@dtstack.com>
2020-07-14 18:46:18 +0800

    feat(breadcrumbRoute): rafactor breadCrumb

---
+++ src/stories/components/breadcrumb/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createMemoryHistory, Router, Route } from 'react-router'
+import BreadcrumbRender from '../../../components/breadcrumb';
+
+const history = createMemoryHistory()
+
+const App = (props) => {
+    return (
+        <div>
+            <BreadcrumbRender
+                routes = {[{ name: 'home', path: '/home' }, { name: 'about', path: '/about' }]}
+                style = {{ background: 'dedede' }}
+            />
+            {props.children}
+        </div>
+    )
+}
+const About = () => <h1>about page</h1>
+const Home = () => <h1>home page</h1>
+
+export default function Breadcrumb () {
+    return (
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+            </Route>
+        </Router>
+    )
+}
