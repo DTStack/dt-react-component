937d1a5cd3161186263ddce0faf100047ff9783d
gaozheng <gaozhengx@163.com>
2020-07-16 11:14:20 +0800

    refactor(add export): add export and delete error #8

--- src/stories/error.stories.tsx
+++ src/stories/error.stories.tsx
@@ -1,55 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import ErrorComponent from '../components/error';
+import Error from '../components/error';
 
 const stories = storiesOf('error 组件', module);
 stories.addDecorator(withKnobs)
-
-class ErrorBoundary extends React.Component<React.FC> {
-    state = {
-        hasError: false
-    }
-    componentDidCatch () {
-        this.setState({ hasError: true })
-    }
-    render () {
-        const { hasError } = this.state;
-        const { children } = this.props;
-        return hasError ? <ErrorComponent /> : children;
-    }
-}
-class TestErrorComponent extends React.Component {
-    componentDidMount () {
-        throw new Error();
-    }
-    render () {
-        return <div>dt-react-component</div>
-    }
-}
 
 stories.add('error', () => (
     <div className='story_wrapper'>
         <h2>何时使用</h2>
         <p>{` 当发生错误时，错误会被componentDidCatch捕获，避免页面崩溃降低用户体验，在这时可以渲染这个组件`}</p>
         <h2>示例</h2>
-        <ErrorBoundary>
-            <TestErrorComponent />
-        </ErrorBoundary>
+        <Error />
     </div>
 ), {
     info: {
-        propTablesExclude: [ErrorBoundary, TestErrorComponent],
+        propTablesExclude: [Error],
         text: `
             代码示例：
             ~~~js
-            import { ErrorComponent } from 'dt-react-component'
+            import { ErrorComponent } from 'dt-react-component';
             class ErrorBoundary extends React.Component<React.FC> {
                 state = {
-                    hasError: false
+                    hasError: false;
                 }
                 componentDidCatch () {
-                    this.setState({ hasError: true })
+                    this.setState({ hasError: true });
                 }
                 render () {
                     const { hasError } = this.state;
