97b30b5376d27bfdbe863bcea55762da2b3691f3
bowenlee <1207107376@qq.com>
2020-09-09 17:45:23 +0800

    perf: optimize .babelrc configuration

--- .babelrc.js
+++ .babelrc.js
@@ -9,30 +9,18 @@ const config = {
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? [
-          './css-plugin',
-          [
-            '@babel/plugin-proposal-decorators',
-            {
-              legacy: true,
-            },
-          ],
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-transform-modules-commonjs',
-        ]
-      : [
-          [
-            '@babel/plugin-proposal-decorators',
-            {
-              legacy: true,
-            },
-          ],
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-transform-modules-commonjs',
-        ],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-modules-commonjs',
+  ],
 };
+
+if (process.env.NODE_ENV === 'production') config.plugins.unshift('./css-plugin');
 module.exports = config;
