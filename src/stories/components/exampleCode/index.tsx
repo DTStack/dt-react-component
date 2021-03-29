227d0c49c1fc7e95884a8da56688e7f585cc7626
Jerome <1732327169@qq.com>
2021-03-29 09:59:58 +0800

    feat(slidepane): add exampleCode (#59)
    Co-authored-by: jingyu <jingyu@dtstack.com>

--- src/stories/components/exampleCode/index.tsx
+++ src/stories/components/exampleCode/index.tsx
@@ -100,7 +100,7 @@ class ExampleContainer extends React.Component<any, any> {
         return dependencies
     }
     handleCodeInCodeSandBox = () => {
-        const { code, otherDependencies } = this.props
+        const { code, otherDependencies, functionCode } = this.props
         const c = `import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
@@ -109,6 +109,7 @@ ${otherDependencies}
 import 'dt-react-component/lib/style/index.css';
 
 class App extends React.Component {
+    ${functionCode}
     render() {
         return (
             ${code}
