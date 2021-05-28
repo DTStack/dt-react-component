4b7020d178cde5bcc98c8a1447dffd564b7c6f95
ChongQianQian <31560521+ChongQianQian@users.noreply.github.com>
2021-05-28 14:45:20 +0800

    feat(load error): add codesandbox code example (#79)
    Co-authored-by: chongqianqian <yunle@dtstack.com>

--- src/stories/loadError.stories.tsx
+++ src/stories/loadError.stories.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
 import LoadError from '../components/loadError';
+import ExampleContainer from './components/exampleCode';
+import { withKnobs } from '@storybook/addon-knobs';
 
 const stories = storiesOf('LoadError 组件', module);
 stories.addDecorator(withKnobs)
 
-stories.add('LoadError', () => (
-    <div className='story_wrapper'>
-        <h2>何时使用</h2>
-        <p>{` 当发生错误时，错误会被componentDidCatch捕获，避免页面崩溃降低用户体验，在这时可以渲染这个组件`}</p>
-        <h2>示例</h2>
+const otherDependencies = `import { LoadError } from 'dt-react-component';`;
+const code = `<LoadError />`;
+stories.add(
+    'LoadError',
+    () => {
+        return (
+            <div className="story_wrapper">
+                <h2>何时使用</h2>
+                <p>
+          当发生错误时，错误会被componentDidCatch捕获，避免页面崩溃降低用户体验，在这时可以渲染这个组件
+                </p>
+                <h2>示例</h2>
+                <ExampleContainer
+                    otherDependencies={otherDependencies}
+                    code={code}
+                    hasCodeSandBox={true}
+                >
+                    <LoadError />
+                </ExampleContainer>
+            </div>
+        );
+    },
+    {
+        info: {
+            propTablesExclude: [LoadError],
+            text: `
+        代码示例：
+        ~~~js
         <LoadError />
-    </div>
-), {
-    info: {
-        propTablesExclude: [LoadError],
-        text: `
-            代码示例：
-            ~~~js
-            import { LoadError } from 'dt-react-component';
-            class ErrorBoundary extends React.Component<React.FC> {
-                state = {
-                    hasError: false;
-                }
-                componentDidCatch () {
-                    this.setState({ hasError: true });
-                }
-                render () {
-                    const { hasError } = this.state;
-                    const { children } = this.props;
-                    return hasError ? <LoadError /> : children;
-                }
-            }
-            ~~~
+        ~~~
         `
+        }
     }
-})
+);
