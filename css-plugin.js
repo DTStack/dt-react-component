7c845d511829544e53b4e0fab1d1145fd7923e9f
chenfeng <profbramble@gmail.com>
2021-07-20 19:08:48 +0800

    fix: gulp to complier (#113)
    * refactor: gulp packaging process update
    
    * chore: add esm folder to git ignore
    
    * chore: babel plugin format correction
    
    * build: adjust typescript config to support both module
    
    * build: add script to support esm and lib
    
    * fix: babel plugin code execution order

--- css-plugin.js
+++ css-plugin.js
@@ -1,15 +1,21 @@
-const t = require('@babel/types');
-module.exports = function() {
+const type = require('@babel/types');
+
+module.exports = function () {
   return {
     visitor: {
       ImportDeclaration(path) {
         const { node } = path;
+
         if (!node) return;
+
         const {
           source: { value: libName },
         } = node;
+        
         if (libName === './style.scss') {
-          path.replaceWith(t.importDeclaration([], t.stringLiteral('./style.css')));
+          path.replaceWith(
+            type.importDeclaration([], type.stringLiteral('./style.css')),
+          );
         }
       },
     },
