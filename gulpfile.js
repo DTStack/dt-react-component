89baebffd85b391db7f5e95816ba89f3842ae7a3
chenfeng <profbramble@gmail.com>
2021-08-02 11:28:14 +0800

    perf: improve the output of esm and lib (#114)
    * refactor: gulp packaging process update
    
    * chore: add esm folder to git ignore
    
    * chore: babel plugin format correction
    
    * build: adjust typescript config to support both module
    
    * build: add script to support esm and lib
    
    * fix: babel plugin code execution order
    
    * feat: add support for default export
    
    * feat: add to-do record
    
    * build: correct ts settings

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ const mergeStream = require('merge-stream');
 const ENV = process.env.NODE_MODULE_ENV;
 
 /**
+ * TODO: When the packaging fails, the scene needs to be restored
+ */
+/**
+
  * existStyleCatalogName ---- The name of the component catalog where the style exists
  * componentCatalogName ---- All component catalog names
  * folderName ---- JavaScript temporary folder name
@@ -26,6 +30,7 @@ function difference(array, arrCompare) {
     return arr.indexOf(v) === arr.lastIndexOf(v);
   });
 }
+
 function unique(arr) {
   return Array.from(new Set(arr));
 }
@@ -50,6 +55,7 @@ function delStyleFile(catalogArr) {
     del([`${basePath}${catalogName}/style.scss`]);
   }
 }
+
 /**
  * traverse files
  * @param isDelete Distinguish between write and delete style file
@@ -131,6 +137,7 @@ async function descriptionDoc() {
         target: 'ES5',
         module: ENV === 'esm' ? 'es6' : 'commonjs',
         skipLibCheck: true,
+        allowSyntheticDefaultImports: true
       }),
     )
     .pipe(dest(`${folderName}/`));
