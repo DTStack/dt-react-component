6d68edc5f172ebd9fcb036ad05310c146b8667c2
“Zaoei” <jingming@dtstack.com>
2021-08-11 16:26:23 +0800

    feat: refactor SearchModal components to increase flexibility

--- src/components/searchModal/index.tsx
+++ src/components/searchModal/index.tsx
@@ -1,79 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-import { Modal, AutoComplete, Input, Row, Col, Select } from 'antd';
+import { Modal, AutoComplete, Input, Row, Col } from 'antd';
 
-const Option = Select.Option;
-const TASK_STATUS = {
-    TASKDESC: 1,
-    TASKNAME: 2
-}
 export interface SearchModalProps {
     visible: boolean;
-    name?: 'notebook';
-    title?: string;
-    onChange?: (value: string, callback: Function, search?: typeof TASK_STATUS) => void;
-    onSelect?: (value: string) => void;
+    dataSource: any[];
+    title?: string | null;
+    placeholder?: string;
+    prefixRender?: React.ReactDOM;
+    onChange?: (value: string) => void;
+    onSelect?: (value: string, option: Object) => void;
     onCancel?: () => void;
     [propName: string]: any;
 }
 class SearchModal extends React.Component<SearchModalProps, any> {
-    state: any = {
-        data: [],
-        search: TASK_STATUS.TASKNAME
-    }
     onCancel = () => {
         this.props.onCancel();
-    }
+    };
+
     onChange = (value: any) => {
         const { onChange } = this.props;
-        const { search } = this.state;
-        if (onChange) {
-            onChange(value, this.changeValue, search)
-        }
-    }
-    searchTask (value: number) {
-        this.setState({ search: value })
-    }
-    onSelect = (value: any) => {
-        this.props.onSelect(value);
-    }
-    changeValue = (values: any) => {
-        this.setState({
-            data: values
-        })
-    }
-    render () {
-        const { data, search } = this.state;
-        const { visible, title, name } = this.props;
+        onChange?.(value);
+    };
+
+    onSelect = (value: any, option: Object) => {
+        this.props?.onSelect(value, option);
+    };
+
+    render() {
+        const { visible, title = '搜索并打开', prefixRender, dataSource = [], bodyStyle,placeholder = '请输入', ...other } = this.props;
         return (
             <Modal
+                {...other}
                 visible={visible}
                 onCancel={this.onCancel}
                 footer={null}
-                title={title || '搜索并打开'}
+                title={title}
             >
                 <Row align="middle" justify="center" type="flex">
-                    { name === 'notebook' && (
-                        <Col span={6} >
-                            <Select style={{ width: '100%', marginTop: '4px' }} value={search} onChange={this.searchTask.bind(this)}>
-                                <Option value={TASK_STATUS.TASKNAME}>任务名称</Option>
-                                <Option value={TASK_STATUS.TASKDESC}>任务描述</Option>
-                            </Select>
-                        </Col>)
-                    }
-                    <Col span={name === 'notebook' ? 18 : 24}>
+                    {prefixRender && <Col span={6} style={{paddingRight: '12px'}}>{prefixRender}</Col>}
+                    <Col span={prefixRender ? 18 : 24}>
                         <AutoComplete
-                            dataSource={data}
-                            style={{ width: '100%', height: '28px', margin: '8px 0' }}
+                            dataSource={dataSource}
+                            style={{ width: '100%', margin: '8px 0' }}
                             onSelect={this.onSelect}
                             onSearch={this.onChange}
                         >
-                            <Input.Search style={{ marginLeft: '8px' }}/>
+                            <Input.Search placeholder={placeholder} />
                         </AutoComplete>
                     </Col>
                 </Row>
             </Modal>
-        )
+        );
     }
 }
 export default SearchModal;
