5ad2231300d80b611e997a1aed6b493ff2293caf
liuxingYuuu <jiawen@dtstack.com>
2022-03-14 20:31:20 +0800

    refactor: compatible handling code for antd 4.x and natter the attention

--- src/stories/formComponent.stories.tsx
+++ src/stories/formComponent.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { PropsTable } from './components/propsTable';
-import { FormItem } from './components/formComponent'
+import FormItem from './components/formComponent'
 
 const stories = storiesOf('FormItem 表单元素', module);
 stories.addDecorator(withKnobs)
@@ -29,14 +29,6 @@ const propDefinitions = [
         required: false,
         description: '控制表单的布局样式',
         defaultValue: '--'
-    }, {
-        property: 'getFieldDecorator',
-        propType: 'Function',
-        required: true,
-        description: (<div className="strory-dt_easy_select_divDesc">
-            和表单进行双向绑定，使用Form.create(options)包裹的组件，会从this.props.form自动获得的属性
-        </div>),
-        defaultValue: '--'
     }
 ]
 
@@ -47,10 +39,16 @@ const IProps = {
     }
 }
 
+const fontStyle = {
+    color: 'red',
+    fontSize: '16px'
+}
+
 stories.add('FormItem', () => (
     <div className='story_wrapper'>
-        <h2>何时使用</h2>
-        <p>{` 当用户需要进行一些表单提交信息时使用，也可以配合封装的BaseForm组件，在模态框中收集用户提交的信息`}</p>
+        <h2 style={fontStyle}>！！！ 请注意 ！！！</h2>
+        <p style={fontStyle}>{` 该组件在 antd 4.x 升级后将会被完全废除，请依赖了此组件的代码自行按照 antd 4.x formItem 规范进行开发！！！`}</p>
+        <p style={fontStyle}>{` 以下示例仅仅作为 3.x 中的常用配置在 4.x 下的常规写法，仅供参考！！！ `}</p>
         <h2>示例</h2>
         <div style={{ width: '300px' }}>
             <FormItem {...IProps} />
@@ -63,18 +61,29 @@ stories.add('FormItem', () => (
         text: `
             代码示例：
             ~~~js
+            import React from 'react';
             import { Form } from 'antd';
-            import RenderFormItem from 'dt-react-component'
+            import { RenderFormItem } from 'dt-react-component';
             const App = (props) => {
-                const { getFieldDecorator } = props.form;
-                const params = { ...props, getFieldDecorator }
+
+                const onFinish = values => {
+                    console.log('Received values of form: ', values);
+                };
+
+                const [form] = Form.useForm();
+                const params = { ...props };
+                const { initialValue } = params;
                 return (
-                    <Form>
+                    <Form
+                        form={form}
+                        onFinish={onFinish}
+                        initialValues={initialValue}
+                    >
                         {RenderFormItem(params)}
                     </Form>
                 )
             }
-            export const FormItem: any = Form.create()(App);
+            export default App;
             ~~~
         `
     }
