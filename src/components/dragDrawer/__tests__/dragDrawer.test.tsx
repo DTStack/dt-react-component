1afc8abb79ac18b5179653a76422f1a30f15ce14
Bell <shiqiharbin@163.com>
2022-08-03 14:13:31 +0800

    feat: add unit cases in dragDrawer

---
+++ src/components/dragDrawer/__tests__/dragDrawer.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import DragDrawer from '../index';
+import { render, fireEvent, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
+
+const prefixCls = 'dtc-drag-drawer';
+describe('test dragDrawer', () => {
+    afterEach(() => {
+        cleanup()
+    })
+    test('disVisible correctly', () => {
+        const { container: wrapper } = render(
+            <DragDrawer getContainer={false} />
+        );
+        expect(wrapper.firstChild).toMatchSnapshot();
+    })
+    test('render correctly', () => {
+        const { container: wrapper } = render(
+            <DragDrawer visible width={400} getContainer={false}>
+                Here is content of Drawer
+            </DragDrawer>
+        );
+        expect(wrapper.firstChild).toMatchSnapshot();
+    });
+    test('render not draggable dragDrawer', () => {
+        const { container: wrapper } = render(
+            <DragDrawer visible getContainer={false} draggable={false}/>
+        );
+        expect(wrapper.firstChild).not.toHaveClass(`${prefixCls}`);
+    })
+
+    // test('call onDrag callback', () => {
+    //     const onDrag = jest.fn();
+    //     const { getByTestId } = render(
+    //         <DragDrawer visible getContainer={false} onDrag= {onDrag}/>
+    //     );
+    //     const draggableElement = getByTestId('dragabble-element');
+    //     fireEvent.drag(draggableElement)
+    //     expect(onDrag).toBeCalled()
+    // })
+})
