f23e3d8ef9e1a9ceba16349bfb0bd01d48c810ee
zaoei <zaoei0108@gmail.com>
2022-09-13 10:58:53 +0800

    refactor: reconstruct ModalWithForm using function components

--- src/components/modalWithForm/index.tsx
+++ src/components/modalWithForm/index.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Modal, Form } from 'antd';
 import { ButtonProps, ButtonType } from 'antd/lib/button';
-import type { FormInstance } from 'antd/es/form';
 
 export interface ModalProps {
     hideModelHandler: () => any;
@@ -20,77 +19,68 @@ export interface ModalProps {
     cancelButtonProps?: ButtonProps;
     notSubmitCloseModal?: boolean;
     layout?: 'horizontal' | 'vertical' | 'inline';
+    children?: React.ReactElement;
+    [key: string]: any;
 }
 
-interface ModalState {
-    form: null | FormInstance;
-}
+const ModalForm = (props: ModalProps) => {
+    const {
+        title,
+        visible,
+        okText = '确定',
+        cancelText = '取消',
+        modelClass,
+        okType,
+        footer,
+        centered,
+        cancelButtonProps,
+        layout = 'vertical',
+        hideModelHandler,
+        children,
+    } = props;
+
+    const [form] = Form.useForm();
+
+    const okHandler = async () => {
+        const { record, notSubmitCloseModal = false, onSubmit, hideModelHandler } = props;
+        try {
+            const values = await form.validateFields();
+            onSubmit(values, record);
+            !notSubmitCloseModal && hideModelHandler();
+        } catch (error) {}
+    };
+
+    const cancelHandler = () => {
+        hideModelHandler();
+        form.resetFields();
+    };
+
+    return (
+        <Modal
+            className={modelClass}
+            title={title}
+            visible={visible}
+            onOk={okHandler}
+            onCancel={cancelHandler}
+            okText={okText}
+            cancelText={cancelText}
+            okType={okType}
+            footer={footer}
+            centered={centered}
+            cancelButtonProps={cancelButtonProps}
+        >
+            <Form form={form} layout={layout}>
+                {React.cloneElement(children, { form: form, ...props })}
+            </Form>
+        </Modal>
+    );
+};
 
 function ModalWithForm(FormComponent: any) {
-    return class ModalForm extends Component<ModalProps, ModalState> {
-        formRef: any = React.createRef();
-        constructor(props: any) {
-            super(props);
-            this.state = {
-                form: null,
-            };
-        }
-
-        componentDidUpdate(prevProps: Readonly<ModalProps>, prevState: Readonly<ModalState>): void {
-            if(this.props.visible && prevProps.visible != this.props.visible){
-                this.formRef.current && this.setState({
-                    form: this.formRef.current,
-                });
-            }
-        }
-
-        okHandler = async () => {
-            const { record, notSubmitCloseModal = false, onSubmit, hideModelHandler } = this.props;
-            try {
-                const values = await this.formRef.current?.validateFields();
-                onSubmit(values, record);
-                notSubmitCloseModal && hideModelHandler();
-            } catch (error) {}
-        };
-        cancelHandler = () => {
-            const { hideModelHandler } = this.props;
-            hideModelHandler();
-            this.formRef.current?.resetFields();
-        };
-        render() {
-            const {
-                title,
-                visible,
-                okText,
-                cancelText,
-                modelClass,
-                okType,
-                footer,
-                centered,
-                cancelButtonProps,
-                layout = 'vertical',
-            } = this.props;
-            const { form } = this.state;
-            return (
-                <Modal
-                    className={modelClass}
-                    title={title}
-                    visible={visible}
-                    onOk={this.okHandler}
-                    onCancel={this.cancelHandler}
-                    okText={okText}
-                    cancelText={cancelText}
-                    okType={okType}
-                    footer={footer}
-                    centered={centered}
-                    cancelButtonProps={cancelButtonProps}
-                >
-                    <Form ref={this.formRef} layout={layout}>
-                        {form && <FormComponent {...this.props} form={form} />}
-                    </Form>
-                </Modal>
-            );
-        }
-    };
+    return (props: ModalProps) => (
+        <ModalForm {...props}>
+            <FormComponent />
+        </ModalForm>
+    );
 }
 export default ModalWithForm;
