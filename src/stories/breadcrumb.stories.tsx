fd0acc1c9f34d75d894883dd7e8e197dc89d28af
mumiao <1270865802zl@gmail.com>
2022-10-09 15:34:13 +0800

    test:  refactor breadcrumb  components and tests (#223)
    * chore: delete unless unitTest cases
    
    * test: refactor breadcrumb test
    
    * fix: delete unless files
    
    * feat: add breadcrumb separator stories
    
    * fix: delete unless files

--- src/stories/breadcrumb.stories.tsx
+++ src/stories/breadcrumb.stories.tsx
@@ -14,7 +14,15 @@ const propDefinitions = [
         required: true,
         description: 'router的路由栈信息',
         defaultValue: 'routes[]'
-    }, {
+    },
+    {
+        property: 'separator',
+        propType: 'ReactNode',
+        required: false,
+        description: '分隔符自定义',
+        defaultValue: '>'
+    },
+    {
         property: 'style',
         propType: 'object',
         required: false,
@@ -26,19 +34,19 @@ const propDefinitions = [
 stories.add('Breadcrumb 面包屑', () => (
     <div className='story_wrapper'>
         <h2>何时使用</h2>
-        <p>{` 当 breadcrumb 组件与 react-router 一起使用时，默认生成的 url 路径是带有 # 的`}</p>
-        <p> {` 依赖需要满足 两级以上的层级结构并需要向上导航的路由时 `}</p>
+        <p> {`routes 需要满足两级以上的层级结构并需要向上导航的路由时 `}</p>
         <h2>示例</h2>
         <BreadcrumbRender/>
     </div>
 ), {
     info: {
         text: `
-            代码示例：
+            代码示例: 
             ~~~js
             <Breadcrumb
                 routes = {[{ name: 'home', path: '/home' }, { name: 'about', path: '/about' }]}
                 style = {{ background: 'dedede' }}
+                separator=">"
             />
             ~~~
         `,
