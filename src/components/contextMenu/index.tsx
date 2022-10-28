c0a60c573f2d6767f1854c9f3009430f6906ebbb
mortalYoung <yangwei1@outlook.com>
2022-10-28 10:47:40 +0800

    refactor: replace contextMenu with dropdown

--- src/components/contextMenu/index.tsx
+++ src/components/contextMenu/index.tsx
@@ -1,132 +1,44 @@
 import React from 'react';
-const contextPrefix = 'dtc-context-menu';
+import { PropsWithChildren } from 'react';
+import { Dropdown, Menu, DropdownProps } from 'antd';
 
-export interface ContextMenuProps {
-    key?: string;
-    targetClassName?: string;
-    onChange?: Function;
-    [propName: string]: any;
-}
-export interface ContextMenuItemProps {
-    key?: string;
-    onClick?: () => void;
-    value?: string;
-    [propName: string]: any;
+interface IMenuProps {
+    key: React.Key;
+    text: string;
+    cb: () => void;
 }
 
-export class ContextMenuItem extends React.Component<ContextMenuItemProps, any> {
-    render() {
-        return (
-            <li {...this.props} className={`${contextPrefix}-context-list_li`}>
-                <a className={`${contextPrefix}-context-list_a`} data-value={this.props.value}>
-                    {this.props.children}
-                </a>
-            </li>
-        );
-    }
+interface IContextMenu
+    extends Pick<
+        DropdownProps,
+        'destroyPopupOnHide' | 'getPopupContainer' | 'placement' | 'overlayClassName'
+    > {
+    data: IMenuProps[];
+    wrapperClassName?: string;
 }
 
-export default class ContextMenu extends React.Component<ContextMenuProps, any> {
-    constructor(props: ContextMenuProps) {
-        super(props);
-        this.toggleMenu = this.toggleMenu.bind(this);
-        this.removeMenu = this.removeMenu.bind(this);
-    }
-    static ContextMenuItem = ContextMenuItem;
+export default function ContextMenu({
+    data,
+    children,
+    wrapperClassName,
+    ...restProps
+}: PropsWithChildren<IContextMenu>) {
+    const menu = (
+        <Menu
+            className="dt-contextMenu-menu"
+            onClick={(item) => {
+                data.find((i) => i.key === item.key)?.cb();
+            }}
+            items={data.map((item) => ({
+                label: item.text,
+                key: item.key,
+            }))}
+        />
+    );
 
-    selfEle: HTMLElement;
-
-    componentDidMount() {
-        document.addEventListener('contextmenu', this.toggleMenu, false);
-        document.addEventListener('click', this.removeMenu, false);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('click', this.removeMenu, false);
-        document.removeEventListener('contextmenu', this.toggleMenu, false);
-    }
-
-    toggleMenu(evt: MouseEvent) {
-        const { targetClassName, onChange } = this.props;
-        const selfEle = this.selfEle;
-        if (!selfEle) return;
-        const parent = this.findParent(evt.target as HTMLElement, targetClassName);
-
-        if (parent) {
-            this.hideAll();
-
-            const style = selfEle.style;
-            style.display = 'block';
-
-            const pointerY = evt.clientY;
-            const pointerX = evt.clientX;
-            const viewHeight = document.body.offsetHeight; // 可视区高度
-            const distanceToBottom = viewHeight - pointerY;
-            const menuHeight = selfEle.offsetHeight;
-            const menuTop = distanceToBottom > menuHeight ? pointerY : pointerY - menuHeight;
-
-            style.cssText = `
-                top: ${menuTop}px;
-                left: ${pointerX}px;
-                display: block;
-            `;
-            if (onChange) {
-                onChange(parent);
-            }
-            evt.preventDefault();
-        }
-    }
-
-    hideAll() {
-        const allEles: any = document.querySelectorAll(`.${contextPrefix}`);
-        for (let i = 0; i < allEles.length; i++) {
-            allEles[i].style.display = 'none';
-        }
-    }
-
-    closeMenu(_evt: MouseEvent) {
-        if (!this.selfEle) return;
-        const style = this.selfEle.style;
-        style.display = 'none';
-    }
-
-    removeMenu(_evt: MouseEvent) {
-        if (!this.selfEle) return;
-        const style = this.selfEle.style;
-        style.display = 'none';
-    }
-
-    findParent(child: HTMLElement, selector: string) {
-        let selectorTemp = selector;
-        try {
-            if (!selectorTemp || !child) return;
-            selectorTemp = selectorTemp.toLowerCase();
-            let node: any = child;
-            while (node) {
-                if (node.nodeType === 1) {
-                    // just hand dom element
-                    const className = node.getAttribute('class');
-                    if (className && className.includes(selectorTemp)) return node;
-                }
-                node = node.parentNode;
-            }
-        } catch (e) {
-            throw new Error(e);
-        }
-        return null;
-    }
-
-    render() {
-        return (
-            <div
-                ref={(e) => {
-                    this.selfEle = e;
-                }}
-                className={contextPrefix}
-                style={{ display: 'none' }}
-            >
-                <ul className={`${contextPrefix}-context-menu_list`}>{this.props.children}</ul>
-            </div>
-        );
-    }
+    return (
+        <Dropdown overlay={menu} trigger={['contextMenu']} destroyPopupOnHide {...restProps}>
+            <span className={wrapperClassName}>{children}</span>
+        </Dropdown>
+    );
 }
--- src/stories/contextMenu.stories.tsx
+++ src/stories/contextMenu.stories.tsx
@@ -2,161 +2,125 @@ import React from 'react';
 import { Tree } from 'antd';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { PropsTable } from './components/propsTable';
-import ContextMenu from '../components/contextMenu';
 import ExampleContainer from './components/exampleCode';
+import ContextMenu from '../components/contextMenu';
+import { PropsTable } from './components/propsTable';
 
 const { TreeNode } = Tree;
-const { ContextMenuItem } = ContextMenu;
 const stories = storiesOf('ContextMenu 右键菜单', module);
 stories.addDecorator(withKnobs);
 
 const propDefinitions = [
     {
-        property: 'targetClassName',
+        property: 'wrapperClassName',
         propType: 'string',
         required: false,
         description: '节点类名，找寻父节点',
         defaultValue: '',
     },
     {
-        property: 'onClick',
-        propType: 'function',
-        required: false,
-        description: '触发事件函数',
-        defaultValue: '',
+        property: 'data',
+        propType: 'array',
+        required: true,
+        description: '菜单项配置',
+        defaultValue: '[]',
     },
 ];
 
 const otherDependencies = `import { ContextMenu } from 'dt-react-component';
 import { Tree } from 'antd';`;
 const code = `<div style={{ position: 'relative' }}>
-                <Tree defaultExpandAll>
-                    <Tree.TreeNode key='0-0' title='folder' className='anchor-experiment-root'>
-                        <Tree.TreeNode key="0-0-0" title='file1' className='anchor-experiment-file' />
-                        <Tree.TreeNode key="0-0-1" title='file2' className='anchor-experiment-file' />
-                    </Tree.TreeNode>
-                </Tree>
-                {this.loopContextMenu()}
-            </div>`;
-const functionCode = `
-        contextMenus = [
-            {
-                targetClassName: 'anchor-experiment-root',
-                menuItems: [{
-                    text: '新建实验',
-                    onClick: (activeNode) => {
-                        console.log('新建实验')
-                    }
-                }, {
-                    text: '新建文件夹',
-                    onClick: (activeNode) => {
-                        console.log('新建文件夹')
-                    }
-                }]
-            }, {
-                targetClassName: 'anchor-experiment-file',
-                menuItems: [{
-                    text: '编辑',
-                    onClick: (activeNode) => {
-                        console.log('编辑')
-                    }
-                }, {
-                    text: '移动',
-                    onClick: (activeNode) => {
-                        console.log('移动')
-                    }
-                }]
-            }
-        ];
-
-       renderContextMenu = (contextMenu) => {
-            if (!contextMenu.menuItems || !contextMenu.menuItems.length) {
-                return null;
-            }
-            return <ContextMenu key={contextMenu.targetClassName} targetClassName={contextMenu.targetClassName}>
-                {contextMenu.menuItems.map((menu) => {
-                    return (<ContextMenu.ContextMenuItem key={menu.text} onClick={() => {
-                        menu.onClick();
-                    }}>{menu.text}</ContextMenu.ContextMenuItem>)
-                })}
+<Tree defaultExpandAll>
+    <TreeNode
+        key="0-0"
+        title={
+            <ContextMenu
+                data={[
+                    {
+                        key: 'create',
+                        text: '新建任务',
+                        cb: () => {},
+                    },
+                    {
+                        key: 'createFolder',
+                        text: '新建文件夹',
+                        cb: () => {},
+                    },
+                    {
+                        key: 'edit',
+                        text: '编辑',
+                        cb: () => {},
+                    },
+                    {
+                        key: 'remove',
+                        text: '删除',
+                        cb: () => {},
+                    },
+                ]}
+            >
+                folder
             </ContextMenu>
         }
+        className="anchor-experiment-root"
+    >
+        <TreeNode
+            key="0-0-0"
+            title={
+                <ContextMenu
+                    data={[
+                        {
+                            key: 'edit',
+                            text: '编辑',
+                            cb: () => {},
+                        },
+                        {
+                            key: 'clone',
+                            text: '克隆',
+                            cb: () => {},
+                        },
+                        {
+                            key: 'remove',
+                            text: '删除',
+                            cb: () => {},
+                        },
+                    ]}
+                >
+                    file1
+                </ContextMenu>
+            }
+            className="anchor-experiment-file"
+        />
+        <TreeNode
+            key="0-0-1"
+            title={
+                <ContextMenu
+                    data={[
+                        {
+                            key: 'edit',
+                            text: '编辑',
+                            cb: () => {},
+                        },
+                        {
+                            key: 'clone',
+                            text: '克隆',
+                            cb: () => {},
+                        },
+                        {
+                            key: 'remove',
+                            text: '删除',
+                            cb: () => {},
+                        },
+                    ]}
+                >
+                    file2
+                </ContextMenu>
+            }
+            className="anchor-experiment-file"
+        />
+    </TreeNode>
+</Tree>
+</div>`;
 
-       loopContextMenu = () => {
-            return this.contextMenus && this.contextMenus.map((contextMenu, index) => {
-                return this.renderContextMenu(contextMenu);
-            })
-        }
-`;
-const contextMenus = [
-    {
-        targetClassName: 'anchor-experiment-root',
-        menuItems: [
-            {
-                text: '新建实验',
-                onClick: (_activeNode: any) => {
-                    console.log('新建实验');
-                },
-            },
-            {
-                text: '新建文件夹',
-                onClick: (_activeNode: any) => {
-                    console.log('新建文件夹');
-                },
-            },
-        ],
-    },
-    {
-        targetClassName: 'anchor-experiment-file',
-        menuItems: [
-            {
-                text: '编辑',
-                onClick: (_activeNode: any) => {
-                    console.log('编辑');
-                },
-            },
-            {
-                text: '移动',
-                onClick: (_activeNode: any) => {
-                    console.log('移动');
-                },
-            },
-        ],
-    },
-];
-function renderContextMenu(contextMenu: any) {
-    if (!contextMenu.menuItems || !contextMenu.menuItems.length) {
-        return null;
-    }
-    return (
-        <ContextMenu
-            key={contextMenu.targetClassName}
-            targetClassName={contextMenu.targetClassName}
-        >
-            {contextMenu.menuItems.map((menu: any) => {
-                return (
-                    <ContextMenuItem
-                        key={menu.text}
-                        onClick={() => {
-                            menu.onClick();
-                        }}
-                    >
-                        {menu.text}
-                    </ContextMenuItem>
-                );
-            })}
-        </ContextMenu>
-    );
-}
-function loopContextMenu() {
-    return (
-        contextMenus &&
-        contextMenus.map((contextMenu: any, _index: any) => {
-            return renderContextMenu(contextMenu);
-        })
-    );
-}
 stories.add(
     'contextMenu',
     () => (
@@ -165,28 +129,97 @@ stories.add(
             <p>任务栏右键菜单操作</p>
             <h2>示例</h2>
             <p>在树目录中右键菜单试试吧</p>
-            <ExampleContainer
-                otherDependencies={otherDependencies}
-                code={code}
-                hasCodeSandBox
-                functionCode={functionCode}
-            >
+            <ExampleContainer otherDependencies={otherDependencies} code={code} hasCodeSandBox>
                 <div style={{ position: 'relative' }}>
                     <Tree defaultExpandAll>
-                        <TreeNode key="0-0" title="folder" className="anchor-experiment-root">
+                        <TreeNode
+                            key="0-0"
+                            title={
+                                <ContextMenu
+                                    data={[
+                                        {
+                                            key: 'create',
+                                            text: '新建任务',
+                                            cb: () => {},
+                                        },
+                                        {
+                                            key: 'createFolder',
+                                            text: '新建文件夹',
+                                            cb: () => {},
+                                        },
+                                        {
+                                            key: 'edit',
+                                            text: '编辑',
+                                            cb: () => {},
+                                        },
+                                        {
+                                            key: 'remove',
+                                            text: '删除',
+                                            cb: () => {},
+                                        },
+                                    ]}
+                                >
+                                    folder
+                                </ContextMenu>
+                            }
+                            className="anchor-experiment-root"
+                        >
                             <TreeNode
                                 key="0-0-0"
-                                title="file1"
+                                title={
+                                    <ContextMenu
+                                        data={[
+                                            {
+                                                key: 'edit',
+                                                text: '编辑',
+                                                cb: () => {},
+                                            },
+                                            {
+                                                key: 'clone',
+                                                text: '克隆',
+                                                cb: () => {},
+                                            },
+                                            {
+                                                key: 'remove',
+                                                text: '删除',
+                                                cb: () => {},
+                                            },
+                                        ]}
+                                    >
+                                        file1
+                                    </ContextMenu>
+                                }
                                 className="anchor-experiment-file"
                             />
                             <TreeNode
                                 key="0-0-1"
-                                title="file2"
+                                title={
+                                    <ContextMenu
+                                        data={[
+                                            {
+                                                key: 'edit',
+                                                text: '编辑',
+                                                cb: () => {},
+                                            },
+                                            {
+                                                key: 'clone',
+                                                text: '克隆',
+                                                cb: () => {},
+                                            },
+                                            {
+                                                key: 'remove',
+                                                text: '删除',
+                                                cb: () => {},
+                                            },
+                                        ]}
+                                    >
+                                        file2
+                                    </ContextMenu>
+                                }
                                 className="anchor-experiment-file"
                             />
                         </TreeNode>
                     </Tree>
-                    {loopContextMenu()}
                 </div>
             </ExampleContainer>
         </div>
@@ -195,30 +228,7 @@ stories.add(
         info: {
             inline: true,
             TableComponent: () => PropsTable({ propDefinitions }),
-            propTablesExclude: [ContextMenuItem, Tree, TreeNode],
-            text: `
-        代码示例：
-        ~~~js
-        function renderContextMenu (contextMenu: any) {
-            if (!contextMenu.menuItems || !contextMenu.menuItems.length) {
-                return null;
-            }
-            return <ContextMenu key={contextMenu.targetClassName} targetClassName={contextMenu.targetClassName}>
-                {contextMenu.menuItems.map((menu: any) => {
-                    return (<ContextMenuItem key={menu.text} onClick={() => {
-                        menu.onClick();
-                    }}>{menu.text}</ContextMenuItem>)
-                })}
-            </ContextMenu>
-        }
-        function loopContextMenu () {
-            return contextMenus && contextMenus.map((contextMenu: any, index: any) => {
-                return renderContextMenu(contextMenu);
-            })
-        }
-        loopContextMenu()
-        ~~~
-      `,
+            propTablesExclude: [Tree, TreeNode, ExampleContainer],
         },
     }
 );
