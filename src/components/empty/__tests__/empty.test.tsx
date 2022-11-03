5daace3fbdcb0547264d7ec5bc97b57485793901
LuckyFBB <38368040+LuckyFBB@users.noreply.github.com>
2022-11-03 20:28:52 +0800

    feat: support more image for empty (#244)

--- src/components/empty/__tests__/empty.test.tsx
+++ src/components/empty/__tests__/empty.test.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
-import Empty, { DEFAULT_BASE64, GREY_BASE64 } from '../index';
+import Empty, { IMG_MAP } from '../index';
 
 describe('Empty', () => {
     test('should support empty success render', () => {
         const wrapper = render(<Empty />);
         expect(wrapper).toMatchSnapshot();
     });
+    it('should support empty image default size', () => {
+        const { container } = render(<Empty />);
+        expect(container.querySelector<HTMLDivElement>('.ant-empty-image')?.style.height).toBe(
+            '80px'
+        );
+    });
     it('should support empty image size should change', () => {
         const { container } = render(<Empty imageStyle={{ height: 20 }} />);
         expect(container.querySelector<HTMLDivElement>('.ant-empty-image')?.style.height).toBe(
             '20px'
+        );
+    });
+
+    it('should support empty image size from iamgeStyle', () => {
+        const { container } = render(<Empty imageStyle={{ height: 40 }} height={100} />);
+        expect(container.querySelector<HTMLDivElement>('.ant-empty-image')?.style.height).toBe(
+            '40px'
         );
     });
 
@@ -24,18 +36,18 @@ describe('Empty', () => {
     it('should support empty render correct img for default type', () => {
         const { container } = render(<Empty />);
         const srcValue = container.querySelector('img').getAttribute('src');
-        expect(srcValue).toEqual(DEFAULT_BASE64);
+        expect(srcValue).toEqual(IMG_MAP['default']);
     });
 
-    it('should support empty render correct img for grey type', () => {
-        const { container } = render(<Empty type="grey" />);
+    it('should support empty render correct img for project type', () => {
+        const { container } = render(<Empty type="project" />);
         const srcValue = container.querySelector('img').getAttribute('src');
-        expect(srcValue).toEqual(GREY_BASE64);
+        expect(srcValue).toEqual(IMG_MAP['project']);
     });
 
     it('should support empty render correct img when have image props', () => {
         const { container } = render(
-            <Empty image="public/iamges/drag-drawer-icon.png" type="grey" />
+            <Empty image="public/iamges/drag-drawer-icon.png" type="project" />
         );
         const srcValue = container.querySelector('img').getAttribute('src');
         expect(srcValue).toEqual('public/iamges/drag-drawer-icon.png');
