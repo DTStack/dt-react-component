860a98199d91df5927936717421a81f48df4e310
mortalYoung <yangwei1@outlook.com>
2022-12-08 20:41:32 +0800

    feat: complete SEO and rest markdown doc

--- .dumi/pages/index/index.tsx
+++ .dumi/pages/index/index.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { Link } from 'dumi';
+import { Link, Helmet } from 'dumi';
 import { Button } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 import './index.scss';
 
 export default function Homepage() {
     return (
-        <div className="dtc-homepage">
-            <h1 className="dtc-homepage-title">dt-react-component</h1>
-            <div className="dtc-homepage-badges">
-                <img src="https://img.shields.io/npm/v/dt-react-component.svg?style=flat" />
-                <img src="http://img.shields.io/npm/dm/dt-react-component.svg?style=flat" />
+        <>
+            <Helmet>
+                <title>dt-react-component</title>
+                <meta property="og:title" content="dt-react-component" data-rh="true"></meta>
+                <meta name="description" content="react-component" />
+                <meta name="author" content="dtinsight UED" />
+                <meta
+                    name="keywords"
+                    content="react,react-component,ui-library,typescript,ant-design"
+                />
+            </Helmet>
+            <div className="dtc-homepage">
+                <h1 className="dtc-homepage-title">dt-react-component</h1>
+                <div className="dtc-homepage-badges">
+                    <img src="https://img.shields.io/npm/v/dt-react-component.svg?style=flat" />
+                    <img src="http://img.shields.io/npm/dm/dt-react-component.svg?style=flat" />
+                </div>
+                <div className="dtc-homepage-description">一个基于 ant design 的组件库</div>
+                <div className="dtc-homepage-btnGroups">
+                    <Link className="ant-btn" to="/guide">
+                        快速开始
+                    </Link>
+                    <Button
+                        icon={<GithubOutlined />}
+                        href="https://github.com/DTStack/dt-react-component"
+                    >
+                        Github
+                    </Button>
+                </div>
             </div>
-            <div className="dtc-homepage-description">一个基于 ant design 的组件库</div>
-            <div className="dtc-homepage-btnGroups">
-                <Link className="ant-btn" to="/guide">
-                    快速开始
-                </Link>
-                <Button
-                    icon={<GithubOutlined />}
-                    href="https://github.com/DTStack/dt-react-component"
-                >
-                    Github
-                </Button>
-            </div>
-        </div>
+        </>
     );
 }
