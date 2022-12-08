71a18e6cda5f67173f14efb793879d79efd8d1df
mortalYoung <yangwei1@outlook.com>
2022-12-08 20:27:08 +0800

    build: add local logo

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     exportStatic: {},
     // TODO 先关闭，后面再看
     ssr: false,
-    logo: 'http://assets.dtstack.com/UEDLanding/Home/logo.png',
-    favicons: ['http://assets.dtstack.com/UEDLanding/Home/logo.png'],
+    logo: '/dt-react-component/logo.png',
+    favicons: ['/dt-react-component/logo.png'],
     links: [{ href: 'https://cdn.jsdelivr.net/npm/antd@4.22.5/dist/antd.css', rel: 'stylesheet' }],
+    clickToComponent: {},
 });
