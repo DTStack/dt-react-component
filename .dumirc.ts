219b4f84391f621b6092375674a2246f7a5adf4c
mortalYoung <yangwei1@outlook.com>
2022-12-09 10:29:36 +0800

    build: add antd style load-on-demand

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,6 +12,14 @@ export default defineConfig({
     ssr: false,
     logo: '/dt-react-component/logo.png',
     favicons: ['/dt-react-component/logo.png'],
-    links: [{ href: 'https://cdn.jsdelivr.net/npm/antd@4.22.5/dist/antd.css', rel: 'stylesheet' }],
+    extraBabelPlugins: [
+        [
+            'import',
+            {
+                libraryName: 'antd',
+                style: true,
+            },
+        ],
+    ],
     clickToComponent: {},
 });
