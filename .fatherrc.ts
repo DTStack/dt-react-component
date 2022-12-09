71ae27848f43d6e97aa0d2f2fbdb6efcf9fce33c
mortalYoung <yangwei1@outlook.com>
2022-12-09 10:33:17 +0800

    build: support cjs production

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'father';
 export default defineConfig({
     // more father config: https://github.com/umijs/father/blob/master/docs/config.md
     esm: { output: 'esm', ignores: ['**/demos/**'], transformer: 'babel' },
+    cjs: { output: 'lib', ignores: ['**/demos/**'], transformer: 'babel' },
     extraBabelPlugins: ['./plugins/replaceRouter'],
 });
