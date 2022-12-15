63489d13b2f3279e7a2691d88fd818c5243e8876
kongshan <kongshan@dtstack.com>
2022-12-15 15:38:34 +0800

    fix: optimized EllipsisText if props has maxWidth can not calculate #257

--- src/components/ellipsisText/index.tsx
+++ src/components/ellipsisText/index.tsx
@@ -57,7 +57,8 @@ export default class EllipsisText extends PureComponent<EllipsisTextProps, State
 
     // The nearest block parent element
     getMaxWidth = (ele: HTMLElement) => {
-        if (!ele) return;
+        // The default maximum width is 120px when the element does not exist
+        if (!ele) return 120;
         const { scrollWidth, offsetWidth, parentElement } = ele;
         // If inline element, find the parent element
         if (scrollWidth === 0) {
@@ -78,6 +79,8 @@ export default class EllipsisText extends PureComponent<EllipsisTextProps, State
 
     onResize = () => {
         const { maxWidth } = this.props;
+        // if props has maxWidth don't have to calculate the text length
+        if (maxWidth) return;
         const ellipsisNode = this.ellipsisRef;
         const rangeWidth = this.getRangeWidth(ellipsisNode);
         const ellipsisWidth = this.getMaxWidth(ellipsisNode.parentElement);
