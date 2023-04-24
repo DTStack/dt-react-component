7b086ada04a4184fe65c1b012902a8a80a20740b
琉易 <liuxy0551@qq.com>
2023-04-24 18:45:16 +0800

    refactor progressLine props (#321)
    * feat: use git cz, simplify deployment command
    
    * refactor: progress line props
    
    * feat: reactNode
    
    ---------
    
    Co-authored-by: liuyi <>
    Co-authored-by: liuyi <liuyi@dtstack.com>

---
+++ src/progressLine/__test__/progressLine.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ProgressLine from '..';
+
+describe('test ProgressLine', () => {
+    it('renders title and percent', () => {
+        const { getByText } = render(<ProgressLine title="Test Title" percent="50%" />);
+        expect(getByText('Test Title')).toBeInTheDocument();
+        expect(getByText('50%')).toBeInTheDocument();
+    });
+
+    it('renders with custom color', () => {
+        const { getByTestId } = render(<ProgressLine color="#FF0000" />);
+        expect(getByTestId('progress-line')).toHaveStyle('background-color: #FF0000');
+    });
+
+    it('renders with custom width', () => {
+        const { getByTestId } = render(<ProgressLine width="500px" />);
+        expect(getByTestId('progress-line-wrap')).toHaveStyle('width: 500px');
+    });
+});
