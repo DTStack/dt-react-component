20183e23f3b58a46406b4bb7a38183c1c2fda9d8
JackWang032 <64318393+JackWang032@users.noreply.github.com>
2023-05-05 10:44:50 +0800

    feat: refactor copyIcon component (#329)
    * feat: refactor copyIcon component
    
    * test: add test case for CopyButton
    
    * feat: rename CopyButton to Copy
    
    * test: fix copy test
    
    ---------
    
    Co-authored-by: jialan <jialan@dtstack.com>

---
+++ src/copy/__tests__/copy.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import Copy from '..';
+
+describe('test Copy', () => {
+    const mockText = 'This is a mock text';
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it('should copy text to clipboard on click', () => {
+        const user = userEvent.setup({ writeToClipboard: true });
+        const mockCopy = jest.fn();
+        const { container } = render(<Copy text={mockText} onCopy={(text) => mockCopy(text)} />);
+        const button = container.querySelector('.dtc-copy');
+
+        expect(button).toBeInTheDocument();
+
+        fireEvent.click(button!);
+        expect(mockCopy).toHaveBeenCalledWith(mockText);
+        user.paste().then((value) => {
+            expect(value).toEqual(mockText);
+        });
+    });
+
+    it('should render with custom button', () => {
+        const user = userEvent.setup({ writeToClipboard: true });
+        const { getByText } = render(
+            <Copy text={mockText} button={<button>测试复制文本</button>} hideTooltip />
+        );
+        const customButton = getByText('测试复制文本');
+
+        expect(customButton).toBeInTheDocument();
+
+        fireEvent.mouseEnter(customButton);
+        setTimeout(() => {
+            expect(document.body.querySelector('.ant-tooltip')).not.toBeInTheDocument();
+        }, 0);
+
+        fireEvent.click(customButton);
+        user.paste().then((value) => {
+            expect(value).toEqual(mockText);
+        });
+    });
+});
