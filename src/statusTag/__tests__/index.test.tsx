1a776f911375af73707604da8a2aed6f8e12b290
kongshan <kongshan@dtstack.com>
2023-05-12 10:30:14 +0800

    fix: statusTag unit test

--- src/statusTag/__tests__/index.test.tsx
+++ src/statusTag/__tests__/index.test.tsx
@@ -15,16 +15,21 @@ describe('test StatusTag suite', () => {
     });
     test('should render StatusTag render correct type', () => {
         const { container } = render(<StatusTag type="run">完成</StatusTag>);
-        expect(container.firstChild!.firstChild).toHaveClass(`${prefixCls}__run`);
+        expect(container.querySelector(`.${prefixCls}__run`)).toBeInTheDocument();
     });
     test('should render StatusTag render correct color', () => {
         const { container } = render(<StatusTag color="#2177b8">完成</StatusTag>);
-        const textWapper = container.firstChild!.firstChild;
+        const textWapper = container.querySelector(`.${prefixCls}__default`);
         expect(textWapper).toHaveStyle({ background: '#2177b8' });
     });
     test('should render StatusTag render correct text', () => {
         const { container } = render(<StatusTag>自定义文案</StatusTag>);
         const textWapper = container.querySelector(`.${prefixCls}__text`)!;
         expect(textWapper.innerHTML).toEqual('自定义文案');
+    });
+    test('should render StatusTag loading', () => {
+        const { container } = render(<StatusTag loading>自定义文案</StatusTag>);
+        const loadingWapper = container.querySelector(`.ant-spin-nested-loading`)!;
+        expect(loadingWapper).toBeInTheDocument();
     });
 });
