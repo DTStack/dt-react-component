5deaaebdca578a30f615d6144256268545a5e0f8
LuckyFBB <38368040+LuckyFBB@users.noreply.github.com>
2023-05-24 02:38:21 +0000

    Deploying to gh-pages from @ DTStack/dt-react-component@f0fd4079e8075fefa913c53855da231ad0a4dffa 🚀

---
+++ markdownRender__demos.ef16f768.async.js
@@ -0,0 +1,35 @@
+"use strict";(self.webpackChunkdt_react_component=self.webpackChunkdt_react_component||[]).push([[6426,7919],{40820:function(c,_,e){e.r(_);var a=e(54306),u=e.n(a),n=e(50959),s=e(50741),t=e(11527);_.default=function(){var m=(0,n.useState)(""),r=u()(m,2),o=r[0],l=r[1];return(0,n.useEffect)(function(){fetch("https://cdn.jsdelivr.net/npm/dt-react-component@3.0.8/CHANGELOG.md",{method:"get"}).then(function(d){return d.text()}).then(l)},[]),(0,t.jsx)("div",{style:{maxHeight:200,overflow:"auto",marginBottom:16},children:(0,t.jsx)(s.MarkdownRender,{value:o})})}},4310:function(c,_,e){e.r(_);var a=e(54306),u=e.n(a),n=e(50959),s=e(50741),t=e(11527),m=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var r=(0,n.useState)(""),o=u()(r,2),l=o[0],d=o[1];return(0,n.useEffect)(function(){d(m)},[]),(0,t.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,t.jsx)(s.MarkdownRender,{dark:!0,value:l})})}},2070:function(c,_,e){e.r(_);var a=e(54306),u=e.n(a),n=e(50959),s=e(50741),t=e(11527),m=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var r=(0,n.useState)(""),o=u()(r,2),l=o[0],d=o[1];return(0,n.useEffect)(function(){d(m)},[]),(0,t.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,t.jsx)(s.MarkdownRender,{value:l})})}}}]);
