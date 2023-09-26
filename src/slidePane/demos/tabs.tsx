9fd9eafbe765a72052573fda3f66f2acb4487014
霜序 <38368040+LuckyFBB@users.noreply.github.com>
2023-09-26 20:03:29 +0800

    feat: support additional features for slidePane (#385)
    * feat: update slidePane for mask
    
    * feat(slidepane): add tabs for slidepane
    
    * feat(slidepane): update motion for slidePane
    
    * feat(slidepane): update ts error and style
    
    * feat(slidepane): update doc and unit test
    
    * fix(slidepane): update props name and edit showMask to mask
    
    * fix(unittest): update unit test
    
    * fix(slidepane): update ts type

---
+++ src/slidePane/demos/tabs.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import { SlidePane } from 'dt-react-component';
+
+export default () => {
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <>
+            <Button onClick={() => setVisible(true)}>打开</Button>
+            <SlidePane
+                visible={visible}
+                onClose={() => setVisible(false)}
+                title={'Title'}
+                tabs={
+                    [
+                        {
+                            key: 'basicInfo',
+                            title: '基本信息',
+                        },
+                        {
+                            key: 'changelog',
+                            title: '变更记录',
+                        },
+                    ] as const
+                }
+                activeKey="changelog"
+            >
+                {(key) => {
+                    switch (key) {
+                        case 'basicInfo':
+                            return <div>基本信息</div>;
+                        case 'changelog':
+                            return <div>变更记录</div>;
+                        default:
+                            break;
+                    }
+                }}
+            </SlidePane>
+        </>
+    );
+};
