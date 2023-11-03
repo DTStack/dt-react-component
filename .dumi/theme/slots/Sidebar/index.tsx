4d86ccf87fb0ac5cc65054e6b9dd88132cd0b122
野迂迂 <yangwei1@outlook.com>
2023-11-03 09:51:27 +0800

    feat: add search component on sidebar

--- .dumi/theme/slots/Sidebar/index.tsx
+++ .dumi/theme/slots/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tooltip } from 'antd';
 import { NavLink, useSidebarData } from 'dumi';
 
@@ -6,21 +6,59 @@ import { useMobile } from '../../hooks';
 import 'dumi/theme-default/slots/Sidebar/index.less';
 import './index.scss';
 
+function filterSideBar(arr: ReturnType<typeof useSidebarData>, search: string) {
+    if (!search) return arr;
+    return arr
+        .map((item) => {
+            const children = item.children.filter((child) =>
+                child.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+            );
+            return { ...item, children };
+        })
+        .filter((i) => i.children.length);
+}
+
 export default function Sidebar() {
     const sidebar = useSidebarData();
     const isMobile = useMobile();
+    const [search, setSearch] = useState('');
+
+    const isRenderSearch = sidebar.some((s) => s.title === '组件');
 
     if (!sidebar) return null;
 
     return (
         <div className="dumi-default-sidebar">
-            {sidebar.map((item, i) => (
+            {isRenderSearch && (
+                <input
+                    className="dt-theme-filter"
+                    placeholder="搜索组件..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            const firstLink = document.querySelector(
+                                '.dumi-default-sidebar-group dd a'
+                            );
+                            if (firstLink) {
+                                (firstLink as HTMLAnchorElement).click();
+                            }
+                        }
+                    }}
+                />
+            )}
+            {filterSideBar(sidebar, isRenderSearch ? search : '').map((item, i) => (
                 <dl className="dumi-default-sidebar-group" key={String(i)}>
                     {item.title && <dt>{item.title}</dt>}
                     {item.children.map((child) => {
                         const link = (
                             <dd key={child.link}>
-                                <NavLink to={child.link} title={child.title} end>
+                                <NavLink
+                                    to={child.link}
+                                    title={child.title}
+                                    end
+                                    onClick={() => setSearch('')}
+                                >
                                     {child.title}
                                 </NavLink>
                             </dd>
