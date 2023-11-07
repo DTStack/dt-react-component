ba85cd5933bde963e3512fc5ae44dcf548121825
霜序 <38368040+LuckyFBB@users.noreply.github.com>
2023-11-07 15:25:35 +0800

    feat: new component contentLayout in v5 (#384)
    * feat(contentlayout): add contentLayout
    
    * feat(contentlayout): update contentLayout export table/header
    
    * feat(contentlayout): update contentLayout info
    
    * feat: update contentLayout md
    
    * feat(contengtlayout): update contentLayout md
    
    * feat(contentlayout): update some props

---
+++ src/contentLayout/demos/basic.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Input } from 'antd';
+import { ColumnsType } from 'antd/lib/table';
+import { ContentLayout } from 'dt-react-component';
+
+export default () => {
+    const columns: ColumnsType<{
+        key: string;
+        name: string;
+        age: number;
+        address: string;
+    }> = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: string) => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+    return (
+        <ContentLayout height="300px">
+            <ContentLayout.Header>
+                <Input.Search placeholder="请输入搜索内容" style={{ width: 256 }} />
+            </ContentLayout.Header>
+            <ContentLayout.Table
+                columns={columns}
+                dataSource={[
+                    {
+                        key: '1',
+                        name: 'John Brown',
+                        age: 32,
+                        address: 'New York No. 1 Lake Park',
+                    },
+                    {
+                        key: '2',
+                        name: 'Jim Green',
+                        age: 42,
+                        address: 'London No. 1 Lake Park',
+                    },
+                    {
+                        key: '3',
+                        name: 'Joe Black',
+                        age: 32,
+                        address: 'Sidney No. 1 Lake Park',
+                    },
+                    {
+                        key: '4',
+                        name: 'Joe Black1',
+                        age: 32,
+                        address: 'Sidney No. 1 Lake Park',
+                    },
+                    {
+                        key: '5',
+                        name: 'Joe Black2',
+                        age: 32,
+                        address: 'Sidney No. 1 Lake Park',
+                    },
+                    {
+                        key: '6',
+                        name: 'Joe Black3',
+                        age: 32,
+                        address: 'Sidney No. 1 Lake Park',
+                    },
+                ]}
+                pagination={false}
+            />
+        </ContentLayout>
+    );
+};
