9dd7a755849e5007f1a4d5a57a5cf692d0687bff
霜序 <38368040+LuckyFBB@users.noreply.github.com>
2023-11-07 15:25:35 +0800

    feat(contentlayout): add contentLayout (#384)
    * feat(contentlayout): add contentLayout
    
    * feat(contentlayout): update contentLayout export table/header
    
    * feat(contentlayout): update contentLayout info
    
    * feat: update contentLayout md
    
    * feat(contengtlayout): update contentLayout md
    
    * feat(contentlayout): update some props

---
+++ src/contentLayout/components.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Table, TableProps } from 'antd';
+import classNames from 'classnames';
+
+import { NAME } from '.';
+
+interface ContentLayoutChildProps {
+    ref?: React.Ref<HTMLDivElement>;
+    children?: React.ReactNode;
+}
+
+type ITableProps<T> = {
+    height?: string;
+    style?: React.CSSProperties;
+    className?: string;
+} & TableProps<T> &
+    Omit<ContentLayoutChildProps, 'ref'>;
+
+export const TableLayout = ({ height, ...otherProps }: ITableProps<any>) => {
+    let lineHeight = 44;
+
+    if (otherProps.footer) {
+        lineHeight = lineHeight * 2;
+    }
+    const scroll: TableProps<any>['scroll'] = otherProps?.scroll ? { ...otherProps.scroll } : {};
+    scroll.y = `calc(${height} - ${lineHeight}px)`;
+
+    return <Table {...otherProps} scroll={scroll} />;
+};
+
+interface HeaderProps extends ContentLayoutChildProps {
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+export const Header = ({ ref, className, style, children }: HeaderProps) => {
+    return (
+        <div className={classNames(`${NAME}__header`, className)} style={style} ref={ref}>
+            {children}
+        </div>
+    );
+};
