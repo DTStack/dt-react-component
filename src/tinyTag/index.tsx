da30fbc120949f535d546a91093a8e7aed95e1e6
beier <beier@dtstack.com>
2024-11-14 11:45:48 +0800

    feat(tinytag): update useSvgWidth hooks && change ref binding dom

--- src/tinyTag/index.tsx
+++ src/tinyTag/index.tsx
@@ -1,53 +1,63 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import './style.scss';
+
+type UseSvgRef = (element: SVGTextElement) => void;
+type UseSvgWidthResult = [UseSvgRef, number, number];
 
 interface ITinyTag extends React.HTMLAttributes<HTMLSpanElement> {
     value: string;
 }
 
-const useSvgWidth = (domRef: React.RefObject<HTMLDivElement>) => {
-    const [width, setWidth] = useState(0);
+const useSvgWidth = (): UseSvgWidthResult => {
+    const [element, ref] = useState<SVGTextElement | null>(null);
+    const [svgWidth, setSvgWidth] = useState(0);
+    const [rectWidth, setRectWidth] = useState(0);
 
-    const getTextWidth = () => {
-        const text = domRef?.current?.getElementsByTagName('text')?.[0];
-        if (!text) return;
+    const getWidth = useCallback(() => {
+        if (!element) return;
         const paddingWidth = 8;
-        const textWidth = Math.round(text.getBoundingClientRect()?.width);
-        const widthSvg = textWidth + paddingWidth;
-        setWidth(widthSvg);
-    };
+        const textWidth = Math.round(element.getBoundingClientRect()?.width);
+        const svgWidth = textWidth + paddingWidth;
+        setSvgWidth(svgWidth);
+        setRectWidth(Math.max(svgWidth - 1, 0));
+    }, [element]);
 
     useEffect(() => {
-        getTextWidth();
-    }, []);
+        getWidth();
+    }, [element]);
 
-    return { width };
+    return [ref, svgWidth, rectWidth];
 };
 
 export default function TinyTag({ value, className, ...restProps }: ITinyTag) {
-    const domRef = useRef<HTMLDivElement>(null);
-    const { width } = useSvgWidth(domRef);
+    const [ref, svgWidth, rectWidth] = useSvgWidth();
     return (
-        <span ref={domRef} className={classNames('dtc-tinyTag', className)} {...restProps}>
+        <span className={classNames('dtc-tinyTag', className)} {...restProps}>
             <svg
-                width={width}
+                width={svgWidth}
                 height="15"
-                viewBox={`0 0 ${width} 15`}
+                viewBox={`0 0 ${svgWidth} 15`}
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
             >
                 <rect
                     x="0.5"
                     y="0.5"
-                    width={width - 1}
+                    width={rectWidth}
                     height="14"
                     rx="1.5"
                     stroke="currentColor"
                     strokeLinejoin="bevel"
                 />
-                <text fill="currentColor" xmlSpace="preserve" fontSize="10" letterSpacing="0px">
+                <text
+                    ref={ref}
+                    fill="currentColor"
+                    xmlSpace="preserve"
+                    fontSize="10"
+                    letterSpacing="0px"
+                >
                     <tspan x="4" y="11.1">
                         {value}
                     </tspan>
