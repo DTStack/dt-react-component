09c4c77247293fb848b3f9db3b26986a699bc98d
Bell <47959147+shiqiWang0@users.noreply.github.com>
2024-11-22 11:16:38 +0800

    feat(tinytag): replace svg to render (#517)
    * feat(tinytag): replace svg to render
    
    * test(tinytag): update tinyTag snapshots
    
    * feat(tinytag): remove default font-family && currentColor in svg
    
    * fix(tinytag): compute padding error && change height 15px
    
    * feat(tinytag): add useSvgWidth
    
    * feat(tinytag): update useSvgWidth hooks && change ref binding dom
    
    ---------
    
    Co-authored-by: beier <beier@dtstack.com>

--- src/tinyTag/demos/basic.tsx
+++ src/tinyTag/demos/basic.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Space } from 'antd';
 import { TinyTag } from 'dt-react-component';
 
+import './style.scss';
+
 export default () => {
     return (
         <Space size={6}>
-            <TinyTag value="袋鼠云" title="袋鼠云" />
-            <TinyTag color="#d1d1d1" value="数据驱动" role="tag" />
-            <TinyTag color="#d56161" value="UED" />
+            <TinyTag value="袋鼠云" />
+            <TinyTag className="data-tag" value="数据驱动" />
+            <TinyTag className="ued-tag" value="UED" />
         </Space>
     );
 };
--- src/tinyTag/index.tsx
+++ src/tinyTag/index.tsx
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import './style.scss';
 
+type UseSvgRef = (element: SVGTextElement) => void;
+type UseSvgWidthResult = [UseSvgRef, number, number];
+
 interface ITinyTag extends React.HTMLAttributes<HTMLSpanElement> {
-    value?: string;
-    color?: string;
+    value: string;
 }
 
-export default function TinyTag({ color, value, className, ...restProps }: ITinyTag) {
+const useSvgWidth = (): UseSvgWidthResult => {
+    const [element, ref] = useState<SVGTextElement | null>(null);
+    const [svgWidth, setSvgWidth] = useState(0);
+    const [rectWidth, setRectWidth] = useState(0);
+
+    const getWidth = useCallback(() => {
+        if (!element) return;
+        const paddingWidth = 8;
+        const textWidth = Math.round(element.getBoundingClientRect()?.width);
+        const svgWidth = textWidth + paddingWidth;
+        setSvgWidth(svgWidth);
+        setRectWidth(Math.max(svgWidth - 1, 0));
+    }, [element]);
+
+    useEffect(() => {
+        getWidth();
+    }, [element]);
+
+    return [ref, svgWidth, rectWidth];
+};
+
+export default function TinyTag({ value, className, ...restProps }: ITinyTag) {
+    const [ref, svgWidth, rectWidth] = useSvgWidth();
     return (
-        <span
-            className={classNames('dtc-tinyTag', className)}
-            style={{ borderColor: color, color }}
-            {...restProps}
-        >
-            {value}
+        <span className={classNames('dtc-tinyTag', className)} {...restProps}>
+            <svg
+                width={svgWidth}
+                height="15"
+                viewBox={`0 0 ${svgWidth} 15`}
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <rect
+                    x="0.5"
+                    y="0.5"
+                    width={rectWidth}
+                    height="14"
+                    rx="1.5"
+                    stroke="currentColor"
+                    strokeLinejoin="bevel"
+                />
+                <text
+                    ref={ref}
+                    fill="currentColor"
+                    xmlSpace="preserve"
+                    fontSize="10"
+                    letterSpacing="0px"
+                >
+                    <tspan x="4" y="11.1">
+                        {value}
+                    </tspan>
+                </text>
+            </svg>
         </span>
     );
 }
