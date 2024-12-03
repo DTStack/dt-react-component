6f32f3dda81273c9349025db03f630abe2ba8f56
野迂迂 <yangwei1@outlook.com>
2024-12-03 15:00:18 +0800

    fix(chat): improve secondary button's style

--- src/chat/button/index.tsx
+++ src/chat/button/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { renderToString } from 'react-dom/server';
 import { Button as AntdButton, ButtonProps } from 'antd';
 import classNames from 'classnames';
 
@@ -9,25 +10,44 @@ interface IButtonProps extends Omit<ButtonProps, 'type'> {
 }
 
 const LINE_GRADIENT_ID = 'LINE_GRADIENT_ID';
+const HOVER_LINE_GRADIENT_ID = 'HOVER_LINE_GRADIENT_ID';
+const CONTAINER_ID = 'CONTAINER_ID';
 
 export default function Button({ type, className, children, ...rest }: IButtonProps) {
+    useEffect(() => {
+        if (!document.querySelector(`#${CONTAINER_ID}`)) {
+            const dom = document.createElement('span');
+            dom.id = CONTAINER_ID;
+            dom.innerHTML = renderToString(
+                <svg width="0" height="0">
+                    <defs>
+                        <linearGradient id={LINE_GRADIENT_ID} gradientTransform="rotate(90)">
+                            <stop offset="0%" stopColor="#00bac6" />
+                            <stop offset="50%" stopColor="#0067ff" />
+                            <stop offset="100%" stopColor="#450fde" />
+                        </linearGradient>
+                        <linearGradient id={HOVER_LINE_GRADIENT_ID} gradientTransform="rotate(90)">
+                            <stop offset="0%" stopColor="#08c4ff" />
+                            <stop offset="50%" stopColor="#4892ff" />
+                            <stop offset="100%" stopColor="#8a61ff" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+            );
+            document.body.append(dom);
+        }
+    }, []);
+
     return (
         <AntdButton
             className={classNames('dtc__aigc__button', className, `dtc__aigc__button--${type}`)}
             ghost
             {...rest}
         >
-            {/* FIXME: 如何解决多个组件渲染多个的问题 */}
-            <svg width="0" height="0">
-                <defs>
-                    <linearGradient id={LINE_GRADIENT_ID} gradientTransform="rotate(90)">
-                        <stop offset="0%" stopColor="#00bac6" />
-                        <stop offset="50%" stopColor="#0067ff" />
-                        <stop offset="100%" stopColor="#450fde" />
-                    </linearGradient>
-                </defs>
-            </svg>
             {children}
+            {type === 'secondary' && !rest.disabled && (
+                <span className="dtc__aigc__mask--secondary" />
+            )}
         </AntdButton>
     );
 }
