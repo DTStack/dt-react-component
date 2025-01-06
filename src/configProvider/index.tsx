5586bcb2b5873456906b39e68b2263cf2d3a42e6
LuckyFBB <976060700@qq.com>
2025-01-06 19:12:10 +0800

    feat(configprovider): migrate locale type to useLocale

--- src/configProvider/index.tsx
+++ src/configProvider/index.tsx
@@ -1,45 +1,6 @@
-import React, { createContext } from 'react';
+import React from 'react';
 
-export interface Locale {
-    locale: string;
-    BlockHeader: { expand: string; collapse: string };
-    Copy: { copied: string; copy: string };
-    Dropdown: { selectAll: string; resetText: string; okText: string };
-    Fullscreen: { exitFull: string; full: string };
-    GlobalLoading: { loading: string };
-    Input: {
-        case: string;
-        precise: string;
-        front: string;
-        tail: string;
-    };
-    LoadError: {
-        please: string;
-        get: string;
-        refresh: string;
-        title: string;
-    };
-    Modal: {
-        okText: string;
-        cancelText: string;
-    };
-    MxGraph: { newNode: string };
-    NotFound: {
-        description: string;
-    };
-    SpreadSheet: {
-        description: string;
-        copy: string;
-        copyCol: string;
-        copyAll: string;
-    };
-}
-
-export interface LocaleContextProps {
-    locale: Locale;
-}
-
-export const LocaleContext = createContext<LocaleContextProps>({ locale: {} as Locale });
+import { LocaleContext, LocaleContextProps } from '../locale/useLocale';
 
 const ConfigProvider = ({
     locale,
--- src/locale/useLocale.tsx
+++ src/locale/useLocale.tsx
@@ -1,8 +1,46 @@
-import { useContext, useMemo } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 
-import type { Locale, LocaleContextProps } from '../configProvider';
-import { LocaleContext } from '../configProvider';
 import defaultLocaleData from './zh-CN';
+
+export interface Locale {
+    locale: string;
+    BlockHeader: { expand: string; collapse: string };
+    Copy: { copied: string; copy: string };
+    Dropdown: { selectAll: string; resetText: string; okText: string };
+    Fullscreen: { exitFull: string; full: string };
+    GlobalLoading: { loading: string };
+    Input: {
+        case: string;
+        precise: string;
+        front: string;
+        tail: string;
+    };
+    LoadError: {
+        please: string;
+        get: string;
+        refresh: string;
+        title: string;
+    };
+    Modal: {
+        okText: string;
+        cancelText: string;
+    };
+    MxGraph: { newNode: string };
+    NotFound: {
+        description: string;
+    };
+    SpreadSheet: {
+        description: string;
+        copy: string;
+        copyAll: string;
+    };
+}
+
+export interface LocaleContextProps {
+    locale: Locale;
+}
+
+export const LocaleContext = createContext<LocaleContextProps>({ locale: {} as Locale });
 
 export type LocaleComponentName = keyof Locale;
 
