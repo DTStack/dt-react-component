f57f32815d0e08c19cc5551f94369238fe163580
mortalYoung <yangwei1@outlook.com>
2025-01-10 18:03:01 +0800

    test(chat): add unit tests

--- src/chat/__tests__/content.test.tsx
+++ src/chat/__tests__/content.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { act, cleanup, render } from '@testing-library/react';
+import { act, cleanup, fireEvent, render } from '@testing-library/react';
+import { produce } from 'immer';
 import '@testing-library/jest-dom/extend-expect';
 
-import Content from '../content';
+import Content, { type IContentRef } from '../content';
 import { Message, Prompt } from '../entity';
 
 class BasePrompt extends Prompt {}
@@ -51,7 +52,25 @@ function generatePrompt() {
     return prompt;
 }
 
-describe('Test Content', () => {
+function generatePromptWithGeneratingMessage() {
+    const prompt = new BasePrompt({
+        id: '1',
+        title: 'What is your name?',
+        messages: [],
+        createdAt: 1736479532239,
+    });
+    prompt.messages.push(
+        new BaseMessage({
+            id: '1',
+            content: 'My Name is dt-react-component',
+            createdAt: 1736479532239,
+            status: 1,
+        })
+    );
+    return prompt;
+}
+
+describe('Test Chat Content', () => {
     let originalRAF: typeof window.requestAnimationFrame;
     beforeEach(() => {
         jest.useFakeTimers();
@@ -123,5 +142,61 @@ describe('Test Content', () => {
         });
 
         expect(fn).toBeCalledTimes(1);
+        expect(fn).toBeCalledWith(100);
+    });
+
+    it('Should always scroll to bottom', () => {
+        let data = generatePromptWithGeneratingMessage();
+        const { container, rerender } = render(
+            <Content data={[data]} placeholder={<div>placeholder</div>} />
+        );
+
+        const ele = container.querySelector('.dtc__aigc__content__container')!;
+        jest.spyOn(ele, 'scrollHeight', 'get')
+            .mockReturnValueOnce(100)
+            .mockReturnValueOnce(400)
+            .mockReturnValue(200);
+        jest.spyOn(ele, 'clientHeight', 'get').mockReturnValue(200);
+
+        const fn = jest.spyOn(ele, 'scrollTop', 'set');
+        expect(fn).not.toBeCalled();
+
+        data = produce(data, (draft) => {
+            draft.messages[0].content += ', I am a developer';
+        });
+        rerender(<Content data={[data]} placeholder={<div>placeholder</div>} />);
+
+        expect(fn).toBeCalledTimes(1);
+        expect(fn).toBeCalledWith(100);
+
+        // Simulate user scroll to top and will not scroll to bottom
+        fireEvent(ele, new Event('scroll'));
+        expect(fn).toBeCalledTimes(1);
+
+        // Simulate user scroll to bottom and will keep sticky at bottom
+        fireEvent(ele, new Event('scroll'));
+        expect(fn).toBeCalledTimes(2);
+        expect(fn).lastCalledWith(200);
+    });
+
+    it('Should get ref', () => {
+        const ref = React.createRef<IContentRef>();
+        const { container } = render(
+            <Content ref={ref} data={[generatePrompt()]} placeholder={<div>placeholder</div>} />
+        );
+
+        const ele = container.querySelector('.dtc__aigc__content__container')!;
+        expect(ref.current?.nativeElement).toBe(ele);
+
+        jest.spyOn(ele, 'scrollHeight', 'get').mockReturnValue(200);
+        const fn = jest.fn();
+        ele.scrollTo = fn;
+
+        act(() => {
+            ref.current?.scrollToBottom();
+        });
+
+        expect(fn).toBeCalledTimes(1);
+        expect(fn).lastCalledWith({ top: 200, left: 0, behavior: 'instant' });
     });
 });
