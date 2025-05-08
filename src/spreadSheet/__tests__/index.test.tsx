d06369262c2c449ba45dd9d4a55ef767ef7a67b1
琉易 <liuxy0551@qq.com>
2025-05-08 19:09:43 +0800

    refactor: add copyTypes and remove showCopyWithHeader 4.x (#545)
    * feat(SpreadSheet): add copyTypes and remove showCopyWithHeader
    
    * docs: demo and changelog
    
    * fix: copy events compatible with old browsers

--- src/spreadSheet/__tests__/index.test.tsx
+++ src/spreadSheet/__tests__/index.test.tsx
@@ -37,7 +37,7 @@ describe('test spreadSheet ', () => {
         const { getByText, unmount } = render(<SpreadSheet data={data} columns={columns} />);
         const cell = getByText('zhangsan');
         fireEvent.contextMenu(cell);
-        const copyBtn = getByText('复制');
+        const copyBtn = getByText('复制值');
         expect(copyBtn).toBeInTheDocument();
         fireEvent.click(copyBtn);
         unmount();
@@ -45,11 +45,27 @@ describe('test spreadSheet ', () => {
 
     test('copy value with header', () => {
         const { getByText, unmount } = render(
-            <SpreadSheet data={data} columns={columns} options={{ showCopyWithHeader: true }} />
+            <SpreadSheet data={data} columns={columns} options={{ copyTypes: ['copyData'] }} />
         );
         const cell = getByText('zhangsan');
         fireEvent.contextMenu(cell);
-        const copyBtn = getByText('复制值以及列名');
+        const copyBtn = getByText('复制值');
+        expect(copyBtn).toBeInTheDocument();
+        fireEvent.click(copyBtn);
+        unmount();
+    });
+
+    test('copy value with header', () => {
+        const { getByText, unmount } = render(
+            <SpreadSheet
+                data={data}
+                columns={columns}
+                options={{ copyTypes: ['copyData', 'copyHeadersAndData'] }}
+            />
+        );
+        const cell = getByText('zhangsan');
+        fireEvent.contextMenu(cell);
+        const copyBtn = getByText('复制列名和值');
         expect(copyBtn).toBeInTheDocument();
         fireEvent.click(copyBtn);
         unmount();
