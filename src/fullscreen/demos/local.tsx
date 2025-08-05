5cf927781344e4a5b9565b29f2d896ae6cf398d9
LuckyFBB <976060700@qq.com>
2025-08-05 11:38:16 +0800

    feat(fullscreen): remove img and use icons
    fix #587

--- src/fullscreen/demos/local.tsx
+++ src/fullscreen/demos/local.tsx
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card } from 'antd';
 import { Fullscreen } from 'dt-react-component';
 
 export default () => {
-    const [targetKey, setTargetKey] = useState('');
-
-    useEffect(() => {
-        setTargetKey('localContainer');
-    }, []);
-
     return (
-        <Card id={targetKey} style={{ background: '#fafafa' }}>
-            <Fullscreen target={targetKey} />
+        <Card id="localContainer" style={{ background: '#fafafa' }}>
+            <Fullscreen target="localContainer" />
             <div style={{ margin: '10px 0', textAlign: 'center' }}>Fullscreen in this</div>
         </Card>
     );
--- src/fullscreen/index.tsx
+++ src/fullscreen/index.tsx
@@ -1,8 +1,9 @@
 import React, { CSSProperties, HTMLAttributes, ReactNode, useEffect, useState } from 'react';
-import { Button } from 'antd';
+import { FullscreenExitOutlined, FullscreenOutlined } from '@dtinsight/react-icons';
 
+import Button from '../button';
 import KeyEventListener from '../keyEventListener';
-import MyIcon from './icon';
+import './index.scss';
 
 const { KeyCombiner } = KeyEventListener;
 declare let document: any;
@@ -26,7 +27,6 @@ interface DocumentWithFullscreen extends Document {
 
 export interface IFullscreenProps
     extends React.DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-    themeDark?: boolean;
     target?: string;
     iconStyle?: CSSProperties;
     fullIcon?: ReactNode;
@@ -35,7 +35,6 @@ export interface IFullscreenProps
 }
 
 export default function Fullscreen({
-    themeDark,
     target,
     fullIcon,
     exitFullIcon,
@@ -173,24 +172,34 @@ export default function Fullscreen({
     };
 
     return (
-        <KeyCombiner
-            onTrigger={handlePressFullScreen}
-            keyMap={{
-                70: true,
-                91: true,
-                16: true,
-            }}
-        >
-            {customIcon ? (
-                <span {...other} onClick={handleFullScreen}>
-                    {customIcon}
-                </span>
-            ) : (
-                <Button onClick={handleFullScreen}>
-                    <MyIcon style={iconStyle} type={isFullScreen} themeDark={themeDark} />
-                    {isFullScreen ? '退出全屏' : '全屏'}
-                </Button>
-            )}
-        </KeyCombiner>
+        <div className="dtc-fullscreen">
+            <KeyCombiner
+                onTrigger={handlePressFullScreen}
+                keyMap={{
+                    70: true,
+                    91: true,
+                    16: true,
+                }}
+            >
+                {customIcon ? (
+                    <span {...other} onClick={handleFullScreen}>
+                        {customIcon}
+                    </span>
+                ) : (
+                    <Button
+                        onClick={handleFullScreen}
+                        icon={
+                            isFullScreen ? (
+                                <FullscreenExitOutlined style={iconStyle} />
+                            ) : (
+                                <FullscreenOutlined style={iconStyle} />
+                            )
+                        }
+                    >
+                        {isFullScreen ? '退出全屏' : '全屏'}
+                    </Button>
+                )}
+            </KeyCombiner>
+        </div>
     );
 }
