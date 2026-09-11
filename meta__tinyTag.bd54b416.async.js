8e9ada716367070e9ab82016989c026a22ac59c4
jin-sir <56628290+jin-sir@users.noreply.github.com>
2026-09-11 10:03:38 +0000

    Deploying to gh-pages from @ DTStack/dt-react-component@cfbda4314fbb2fc8c3f3b4181d41dda0318d6486 🚀

---
+++ meta__tinyTag.bd54b416.async.js
@@ -0,0 +1,93 @@
+"use strict";(self.webpackChunkdt_react_component=self.webpackChunkdt_react_component||[]).push([[4984],{6867:(function(e,t,n){n.r(t),n.d(t,{demos:function(){return d}});var a=n(30758),d={"tinytag-demo-basic":{component:a.memo(a.lazy(function(){return Promise.all([n.e(8848),n.e(5808),n.e(1585),n.e(4896),n.e(3069),n.e(9495),n.e(1073),n.e(5150),n.e(8998),n.e(5385),n.e(6217),n.e(2441),n.e(7413),n.e(5619),n.e(9275),n.e(198),n.e(8578),n.e(9389),n.e(5175)]).then(n.bind(n,71947))})),asset:{type:"BLOCK",id:"tinytag-demo-basic",refAtomIds:["tinyTag"],dependencies:{"index.tsx":{type:"FILE",value:n(33031).A},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-react-component":{type:"NPM",value:"5.1.0"},"./style.scss":{type:"FILE",value:n(26495).A}},entry:"index.tsx",title:"\u57FA\u7840\u4F7F\u7528"},routeId:"components/tinyTag/index",context:void 0,renderOpts:void 0},"tinytag-demo-hidden":{component:a.memo(a.lazy(function(){return Promise.all([n.e(8848),n.e(5808),n.e(1585),n.e(4896),n.e(3069),n.e(9495),n.e(1073),n.e(5150),n.e(8998),n.e(5385),n.e(6217),n.e(2441),n.e(7413),n.e(5619),n.e(9275),n.e(198),n.e(8578),n.e(9389),n.e(5175)]).then(n.bind(n,61071))})),asset:{type:"BLOCK",id:"tinytag-demo-hidden",refAtomIds:["tinyTag"],dependencies:{"index.tsx":{type:"FILE",value:n(5239).A},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"4.24.16"},"dt-react-component":{type:"NPM",value:"5.1.0"}},entry:"index.tsx",title:"\u9690\u85CF\u6E32\u67D3"},routeId:"components/tinyTag/index",context:void 0,renderOpts:void 0},"tinytag-demo-table":{component:a.memo(a.lazy(function(){return Promise.all([n.e(8848),n.e(5808),n.e(1585),n.e(4896),n.e(3069),n.e(9495),n.e(1073),n.e(5150),n.e(8998),n.e(5385),n.e(6217),n.e(2441),n.e(7413),n.e(5619),n.e(9275),n.e(198),n.e(8578),n.e(9389),n.e(5175)]).then(n.bind(n,23853))})),asset:{type:"BLOCK",id:"tinytag-demo-table",refAtomIds:["tinyTag"],dependencies:{"index.tsx":{type:"FILE",value:n(55701).A},react:{type:"NPM",value:"18.3.1"},"dt-react-component":{type:"NPM",value:"5.1.0"}},entry:"index.tsx",title:"\u8868\u683C\u4F7F\u7528"},routeId:"components/tinyTag/index",context:void 0,renderOpts:void 0}}}),30595:(function(e,t,n){n.r(t);const a=[{value:"TinyTag \u7EC4\u4EF6\u901A\u5E38\u7528\u4E8E\u5728\u67D0\u4E9B\u6587\u6848\u540E\u9762\uFF0C\u7528\u4E8E\u6807\u8BC6\u5F53\u524D\u884C\u6570\u636E\u7684\u5206\u7C7B",paraId:0,tocIndex:1},{value:"\u53C2\u6570",paraId:1,tocIndex:7},{value:"\u8BF4\u660E",paraId:1,tocIndex:7},{value:"\u7C7B\u578B",paraId:1,tocIndex:7},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:7},{value:"value",paraId:1,tocIndex:7},{value:"\u6807\u7B7E\u6587\u6848",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"type",paraId:1,tocIndex:7},{value:"\u6807\u7B7E\u7C7B\u578B",paraId:1,tocIndex:7},{value:"default | fill",paraId:1,tocIndex:7},{value:"default",paraId:1,tocIndex:7},{value:"color",paraId:1,tocIndex:7},{value:"\u6807\u7B7E\u5B57\u4F53\u989C\u8272\uFF0C\u9ED8\u8BA4\u6A21\u5F0F\u4E0B\u540C\u65F6\u4F5C\u4E3A\u8FB9\u6846\u989C\u8272",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"bgColor",paraId:1,tocIndex:7},{value:"\u6807\u7B7E\u80CC\u666F\u8272\uFF0C\u4EC5\u5728 ",paraId:1,tocIndex:7},{value:"type",paraId:1,tocIndex:7},{value:" \u4E3A ",paraId:1,tocIndex:7},{value:"fill",paraId:1,tocIndex:7},{value:" \u7684\u60C5\u51B5\u4E0B\u751F\u6548",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"className",paraId:1,tocIndex:7},{value:"class \u540D\u79F0",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"\u7EE7\u627F\u539F\u751F ",paraId:2},{value:"<span>",paraId:2},{value:" \u6807\u7B7E\u7684\u5C5E\u6027\uFF0C\u652F\u6301\u6240\u6709 ",paraId:2},{value:"React.HTMLAttributes<HTMLSpanElement>",paraId:2}];n.d(t,["texts",0,a])}),33031:(function(e,t){t.A=`import React from 'react';
+import { Space } from 'antd';
+import { TinyTag } from 'dt-react-component';
+
+import './style.scss';
+
+export default () => {
+    return (
+        <Space size={6}>
+            <TinyTag value="\u888B\u9F20\u4E91" />
+            <TinyTag className="data-tag" value="\u6570\u636E\u9A71\u52A8" />
+            <TinyTag className="ued-tag" value="UED" />
+            <TinyTag type="fill" bgColor="#D56161" color="#fff" value="\u5B9E\u4F53\u6807\u7B7E" />
+        </Space>
+    );
+};
+`}),5239:(function(e,t){t.A=`import React, { useState } from 'react';
+import { Button, Space } from 'antd';
+import { TinyTag } from 'dt-react-component';
+
+export default () => {
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <Space direction="vertical" size={8}>
+            <Button type="primary" onClick={() => setVisible(!visible)}>
+                {visible ? '\u9690\u85CF\u6807\u7B7E' : '\u663E\u793A\u6807\u7B7E'}
+            </Button>
+            <div style={{ display: visible ? 'block' : 'none' }}>
+                <TinyTag value="\u9690\u85CF\u6E32\u67D3\u540E\u663E\u793A\u7684\u6807\u7B7E" />
+            </div>
+        </Space>
+    );
+};
+`}),26495:(function(e,t){t.A=`.data-tag {
+    color: #D1D1D1;
+    &:hover {
+        color: #D56161;
+    }
+}
+
+.ued-tag {
+    color: #D56161;
+}
+`}),55701:(function(e,t){t.A=`import React from 'react';
+import { Table, TinyTag } from 'dt-react-component';
+
+interface DataType {
+    id: number;
+    name: string;
+    age: number;
+    type: 0 | 1 | 2;
+    address: string;
+}
+
+const dataSource = [
+    { id: 1, type: 0, name: 'ZhangSan', age: 17, address: 'New York No. 1 Lake Park' },
+    { id: 2, type: 1, name: 'LiSi', age: 17, address: 'Bei Jing No. 1 Lake Park' },
+    { id: 3, type: 2, name: 'WangWu', age: 17, address: 'Zhe Jiang No. 1 Lake Park' },
+] as DataType[];
+
+const typpings = ['\u536B\u751F\u59D4\u5458', '\u73ED\u957F', '\u56E2\u59D4\u4E66\u8BB0'];
+
+export default () => {
+    return (
+        <Table<DataType>
+            rowKey="id"
+            columns={[
+                {
+                    dataIndex: 'name',
+                    title: 'Name',
+                    render(text, record) {
+                        return (
+                            <span>
+                                {text}
+                                <TinyTag value={typpings[record.type]} />
+                            </span>
+                        );
+                    },
+                },
+                {
+                    dataIndex: 'age',
+                    title: 'Age',
+                },
+                { dataIndex: 'address', title: 'Address' },
+            ]}
+            dataSource={dataSource}
+            pagination={false}
+            bordered
+        />
+    );
+};
+`})}]);
